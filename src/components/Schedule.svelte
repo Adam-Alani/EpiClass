2e6aba936abcf01b1a45f9fd9190904af6542265
Adam <adam.alani@epita.fr>
2021-01-08 23:59:53 +0100

    button hover

--- src/components/Schedule.svelte
+++ src/components/Schedule.svelte
@@ -65,7 +65,7 @@
 
     <div class="h-16 hidden mx-2 lg:flex justify-center  text-center items-center">
         <div class="w-2/12">
-        <a role="button"  on:click={()=> {increaseDay(true)}} class="rounded-full border-transparent hover:bg-green-300 dgray-text px-0 mx-0 ">
+        <a role="button"  on:click={()=> {increaseDay(true)}} class="float-left rounded-full border-transparent hover:bg-green-300 dgray-text ">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-left-square" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/></svg>
         </a>
         </div>
@@ -74,7 +74,7 @@
         <h1 class="flex-none text-xl dgray-text text-center cursor-default select-none"><strong>{$weekDays[$currDayStr]}</strong></h1>
         </div>
         <div class="w-2/12">
-        <a role="button" on:click={()=> {increaseDay(false)}}  class="float-right rounded-full border-transparent hover:bg-green-300 dgray-text C:\Programming\EpiCal\src\components\Schedule.svelte  ">
+        <a role="button" on:click={()=> {increaseDay(false)}}  class="float-right rounded-full border-transparent hover:bg-green-300 dgray-text ">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/></svg>
         </a>
         </div>
